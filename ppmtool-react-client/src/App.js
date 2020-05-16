2191c35517251fd44412d13922e995ed38168840
Mauricio Cavallini <mauricio1acp@hotmail.com>
2020-05-16 21:01:08 +0200

    to production

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -4,15 +4,41 @@ import Dashboard from "./Components/Dashboard";
 import Header from "./Components/Layout/Header";
 //Enabling boostrap
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AddProject from "./Components/Project/AddProject";
 import { Provider } from "react-redux";
 import store from "./store";
 import UpdateProject from "./Components/Project/UpdateProject";
 import ProjectBoard from "./Components/ProjectBoard/ProjectBoard";
 import AddProjectTask from "./Components/ProjectBoard/ProjectTasks/AddProjectTask";
- 
+import UpdateProjectTask from "./Components/ProjectBoard/ProjectTasks/UpdateProjectTask";
+import Landing from "./Components/Layout/Landing";
+import Register from "./Components/UserManagement/Register";
+import Login from "./Components/UserManagement/Login";
+import jwt_decode from "jwt-decode";
+import setJWTToken from "./security Utils/setJWTToken";
+import { SET_CURRENT_USER } from "./actions/types";
+import { logout } from "./actions/securityActions";
+import SecuredRoute from "./security Utils/SecureRoute";
 
+const jwtToken = localStorage.jwtToken;
+
+if (jwtToken) {
+  setJWTToken(jwtToken);
+  const decoded_jwtToken = jwt_decode(jwtToken);
+  store.dispatch({
+    type: SET_CURRENT_USER,
+    payload: decoded_jwtToken,
+  });
+
+  const currentTime = Date.now() / 1000;
+  if (decoded_jwtToken.exp < currentTime) {
+    //handle logout
+    store.dispatch(logout());
+
+    window.location.href = "/";
+  }
+}
 
 function App() {
   return (
@@ -20,15 +46,49 @@ function App() {
       <Router>
         <div className="App">
           <Header></Header>
-          <Route exact path="/dashboard" component={Dashboard}></Route>
-          <Route exact path="/addProject" component={AddProject}></Route>
-          <Route
-            exact
-            path="/updateProject/:id"
-            component={UpdateProject}
-          ></Route>
-          <Route exact path="/projectBoard/:id" component={ProjectBoard}></Route>
-          <Route exact path="/addProjectTask/:id" component={AddProjectTask}></Route>
+
+          {
+            //Public Routes
+          }
+
+          <Route exact path="/" component={Landing}></Route>
+          <Route exact path="/register" component={Register}></Route>
+          <Route exact path="/login" component={Login}></Route>
+          {
+            //Private Routes
+          }
+          <Switch>
+            <SecuredRoute
+              exact
+              path="/dashboard"
+              component={Dashboard}
+            ></SecuredRoute>
+            <SecuredRoute
+              exact
+              path="/addProject"
+              component={AddProject}
+            ></SecuredRoute>
+            <SecuredRoute
+              exact
+              path="/updateProject/:id"
+              component={UpdateProject}
+            ></SecuredRoute>
+            <SecuredRoute
+              exact
+              path="/projectBoard/:id"
+              component={ProjectBoard}
+            ></SecuredRoute>
+            <SecuredRoute
+              exact
+              path="/addProjectTask/:id"
+              component={AddProjectTask}
+            ></SecuredRoute>
+            <SecuredRoute
+              exact
+              path="/updateProjectTask/:backlog_id/:pt_id"
+              component={UpdateProjectTask}
+            ></SecuredRoute>
+          </Switch>
         </div>
       </Router>
     </Provider>
---
+++ ppmtool-react-client/src/actions/securityActions.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import setJWTToken from "../security Utils/setJWTToken";
+import jwt_decode from "jwt-decode";
+
+
+export const createNewUser = (newUser, history) => async (dispatch) => {
+  try {
+    await axios.post("http://localhost:8080/api/users/register", newUser);
+    history.push("/login");
+    dispatch({
+      type: GET_ERRORS,
+      payload: {},
+    });
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data,
+    });
+  }
+};
+
+export const login = (LoginRequest) => async (dispatch) => {
+  try {
+    //post => Login Request
+    const res = await axios.post(
+      "http://localhost:8080/api/users/login",
+      LoginRequest
+    );
+    //extract token from res.data
+    const { token } = res.data;
+    //store the token in the LocalStorage
+    localStorage.setItem("jwtToken", token);
+    //set our token in our header
+    setJWTToken(token);
+    //decode token on React
+    const decoded = jwt_decode(token);
+    //dispatch to our securityReducer
+    dispatch({
+      type: SET_CURRENT_USER,
+      payload: decoded,
+    });
+  } catch (error) {
+      dispatch({
+          type:GET_ERRORS,
+          payload:error.response.data
+      })
+  }
+};
+
+export const logout=()=> dispatch => {
+    localStorage.removeItem("jwtToken");
+    setJWTToken(false)
+    dispatch({
+        type: SET_CURRENT_USER,
+        payload: {},
+      });
+}
